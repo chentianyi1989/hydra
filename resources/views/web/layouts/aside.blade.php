7b5a26a56e6f59f238139797639cee55a978bbc8
chenty <chenty@chenty-PC>
2018-04-08 23:29:55 +0800

    1

--- resources/views/web/layouts/aside.blade.php
+++ resources/views/web/layouts/aside.blade.php
@@ -1,28 +1,3 @@
-<!-- <div class="aside"> -->
-<!--     <div> -->
-<!--         <ul> -->
-<!--             <li> -->
-<!--             	<script language="javascript" src="http://api.pop800.com/800.js?n=347590&s=01&p=l&l=cn"></script> 
-            	<div style="display:none;"><a href="">在线客服</a></div> -->
-<!--            <a href="javascript:;" onclick="javascript:window.open('http://api1.pop800.com/chat/291222','','width=1024,height=768')"><img src="{{ asset('/web/images/xierdun/aside1.png') }}" alt=""></a>
-<!--                 <a href="javascript:;" onclick="javascript:window.open('http://wpa.qq.com/msgrd?v=3&uin=2697173363&site=qq&menu=yes','','width=1024,height=768')"> -->
-<!--                     <img src="{{ asset('/web/m3/images/aside_r1.png') }}" tppabs="http://mb3.uc697.com/web/images/aside_r1.png" alt=""> -->
-<!--                 </a> -->
-<!--             </li> -->
-<!--             <li> -->
-<!--                 <a href="javascript:;" class="daili_apply"> -->
-<!--                     <img src="{{ asset('/web/m3/images/aside_r4.png') }}" tppabs="http://mb3.uc697.com/web/images/aside_r4.png" alt=""> -->
-<!--                 </a> -->
-<!--             </li> -->
-<!--             <li> -->
-<!--                 <a href="javascript:;"> -->
-<!--                     <img src="{{ asset('/web/m3/images/aside_r3.png') }}" tppabs="http://mb3.uc697.com/web/images/aside_r3.png" alt=""> -->
-<!--                 </a> -->
-<!--                 <img class="aside_erweima" src="" alt=""> -->
-<!--             </li> -->
-<!--         </ul> -->
-<!--     </div> -->
-<!-- </div> -->
 
 
 

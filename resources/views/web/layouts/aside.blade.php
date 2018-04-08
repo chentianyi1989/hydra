7fd450344e6b48f7d14e9634ecf42b0fc57bb147
chenty <chenty@chenty-PC>
2018-04-08 23:16:59 +0800

    1

--- resources/views/web/layouts/aside.blade.php
+++ resources/views/web/layouts/aside.blade.php
@@ -31,8 +31,5 @@
         <li>
             <a href="javascript:;" onclick="javascript:window.open('http://comm9.pop800.com/web800/c.do?n=347590&type=0&l=cn&w=0','','width=1024,height=768')"><img src="{{ asset('/web/images/xierdun/aside1.png') }}" alt=""></a>
         </li>
-         <li> 
-            <a href="javascript:;" onclick="javascript:window.open('http://wpa.qq.com/msgrd?v=3&uin=181812592&site=qq&menu=yes','','width=1024,height=768')"><img src="{{ asset('/web/images/xierdun/aside2.png') }}" alt=""></a>
-         </li> 
     </ul>
 </div>
